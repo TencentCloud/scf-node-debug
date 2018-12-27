4677ababadfb50d00a0b220283f7140b77236e9f
louiswu <louiswu@tencent.com>
2018-12-27 10:12:45 +0800

    test(test): correct temoDemo
    correct testDemo

--- test/mock/correctDemo.js
+++ test/mock/correctDemo.js
@@ -1,9 +1,8 @@
 'use strict'
 exports.main_handler = (event, context, callback) => {
-  event.request['header']['user-agent'] =
-    event.request['header']['user-agent'] +
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
-  console.log(event)
-  setTimeout(() => {}, 1 * 1000)
-  return 'hello world'
+  let rpa = require('request-promise-any')
+  rpa('https://gank.io/api/xiandu/categories').then(json => {
+    console.log(json)
+  })
+  return 123
 }
